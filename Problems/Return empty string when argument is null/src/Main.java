d21cb791571b8a6f5536ec22935bdc9fdb85448a
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-12-23 02:14:35 +0100

    Add solved problem 'Return empty string when argument is null'

---
+++ Problems/Return empty string when argument is null/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+    public static String toUpperCase(String str) {
+        return str == null ? "" : str.toUpperCase(Locale.ENGLISH);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        line = "none".equalsIgnoreCase(line) ? null : line;
+        System.out.println(toUpperCase(line));
+    }
+}
